4bd54578bc2a3daead247e5c5c3f5d17693e216d
divyarajsinhsindhav-tss <divyarajsinh.sindhav@tssconsultancy.com>
2026-04-13 19:13:38 +0530

    Template Form

---
+++ templateForm/src/app/app.ts
@@ -0,0 +1,39 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { Student } from './student';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('templateForm');
+
+  student: Student[] = [];
+  hobbies: string[] = [];
+  sendData(data: NgForm) {
+    const newStudent:Student = {
+      id: Date.now(),
+      name: data.value.name,
+      email: data.value.email,
+      country: data.value.country,
+      hobbies: this.hobbies
+    }
+    this.student.push(newStudent);
+  }
+
+  onHobbyChange(event: any) {
+    const value = event.target.nextSibling.textContent.trim();
+    const checked = event.target.checked;
+
+    if (checked) {
+      this.hobbies.push(value);
+    } else {
+      this.hobbies = this.hobbies.filter(h => h !== value);
+    }
+  }
+  
+}
---
+++ templateForm/src/app/student.ts
@@ -0,0 +1,7 @@
+export interface Student {
+    id: number,
+    name: string,
+    email: string,
+    country: string,
+    hobbies: string[]
+}
